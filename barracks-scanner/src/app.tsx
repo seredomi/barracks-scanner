84cec6a611cb70a5da0ae05cee2abd42554f68ef
seredomi <serenomdominguez@gmail.com>
2024-02-09 23:13:54 +0900

    converting to chakra ui

--- barracks-scanner/src/app.tsx
+++ barracks-scanner/src/app.tsx
@@ -1,4 +1,13 @@
 import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = createRoot(document.body);
-root.render(<h1>Scanner</h1>);
+root.render(App());
+
+function App() {
+    return (
+        <ChakraProvider>
+            <h1>Scanner</h1>
+        </ChakraProvider>
+    )
+}
