b9fcbc2011a22bc2ee0aa89ec674d501fe9ef62d
seredomi <serenomdominguez@gmail.com>
2024-02-10 21:04:22 +0900

    basic nav components in place. need to configure routing

--- barracks-scanner/src/app.jsx
+++ barracks-scanner/src/app.jsx
@@ -1,15 +1,52 @@
 import * as React from 'react';
-import { Header, HeaderName } from '@carbon/react';
-// import './app.scss';
+import ReactDOM from 'react-dom/client';
+import { Content, HeaderContainer, Header, HeaderName, SideNavItems, SideNav, SideNavLink } from '@carbon/react';
+import { ScanBarcode, User, History } from 'lucide-react';
 import { createRoot } from 'react-dom/client';
+
+import IdScannerPage from './pages/id-scanner.jsx';
+import PersonnelPage from './pages/personnel.jsx';
+import ScanHistoryPage from './pages/scan-history.jsx';
 
 const root = createRoot(document.body);
 root.render(App());
 
+
+function Title() {
+    return (
+        <Header aria-label="Window Header">
+            <HeaderName prefix="6070">
+                Barracks Personnel Identifier
+            </HeaderName>
+        </Header>
+    )
+
+}
+
+function NavBar() {
+    return (
+        <SideNav
+            isFixedNav
+            expanded={true}
+            isChildOfHeader={false}
+            aria-label="Side navigation"
+        >
+            <SideNavItems>
+                    <SideNavLink renderIcon={ScanBarcode} large>ID Scanner</SideNavLink>
+                <SideNavLink renderIcon={User} large>Personnel</SideNavLink>
+                <SideNavLink renderIcon={History} large>Scan History</SideNavLink>
+            </SideNavItems>
+        </SideNav>
+    )
+}
 function App() {
-    return <Header aria-label="Header for Our Skeleton App">
-        <HeaderName href="https://react.carbondesignsystem.com/" prefix="Carbon Design System">
-        Header for Our Skeleton App
-        </HeaderName>
-    </Header>;
+    return (
+        <>
+            <Title />
+            <NavBar />
+            <Content>
+                <IdScannerPage />
+            </Content>
+        </>
+    )
 }
