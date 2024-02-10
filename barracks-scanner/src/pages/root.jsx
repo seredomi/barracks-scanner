246a25b73c32eedbd327f0ee438eff42f23618ed
seredomi <serenomdominguez@gmail.com>
2024-02-11 00:02:14 +0900

    proper button / selection behavior

--- barracks-scanner/src/pages/root.jsx
+++ barracks-scanner/src/pages/root.jsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Content, HeaderContainer, Header, HeaderName, SideNavItems, SideNav, SideNavLink } from '@carbon/react';
 import { Outlet } from 'react-router-dom';
 import { ScanBarcode, User, History } from 'lucide-react';
 
 function Title() {
+    const buildingNo = useState(6070);
     return (
         <Header aria-label="Window Header" className="globalHeader">
-            <HeaderName prefix="6070">
+            <HeaderName prefix={buildingNo}>
                 Barracks Personnel Identifier
             </HeaderName>
         </Header>
@@ -16,6 +18,11 @@ function Title() {
 }
 
 function NavBar() {
+    const [selected, setSelected] = useState('id-scanner');
+    const handleIdScannerClick = () => { setSelected('id-scanner'); }
+    const handlePersonnelClick = () => { setSelected('personnel'); }
+    const handleScanHistoryClick = () => { setSelected('scan-history'); }
+
     return (
         <SideNav
             isFixedNav
@@ -24,9 +31,24 @@ function NavBar() {
             aria-label="Side navigation"
         >
             <SideNavItems>
-                <SideNavLink renderIcon={ScanBarcode} aref="id-scanner" large isActive={true}>ID Scanner</SideNavLink>
-                <SideNavLink renderIcon={User} aref="personnel" large>Personnel</SideNavLink>
-                <SideNavLink renderIcon={History} aref="scan-history" large>Scan History</SideNavLink>
+                <SideNavLink
+                    isActive={selected === 'id-scanner'}
+                    onClick={handleIdScannerClick}
+                    renderIcon={ScanBarcode} large >
+                    ID Scanner
+                </SideNavLink>
+                <SideNavLink
+                    isActive={selected === 'personnel'} 
+                    onClick={handlePersonnelClick}
+                    renderIcon={User} large >
+                    Personnel
+                </SideNavLink>
+                <SideNavLink
+                    isActive={selected === 'scan-history'} 
+                    onClick={handleScanHistoryClick}
+                    renderIcon={History} large >
+                    Scan History
+                </SideNavLink>
             </SideNavItems>
         </SideNav>
     )
