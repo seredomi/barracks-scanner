0a7efbf66653c2885e2c8ace4b00209806fbc6d1
seredomi <serenomdominguez@gmail.com>
2024-02-10 09:20:41 +0900

    scrapped again :/

--- barracks-scanner/src/renderer.ts
+++ barracks-scanner/src/renderer.ts
@@ -27,5 +27,29 @@
  */
 
 import './index.css';
+import { useState } from 'react';
+import { TamaguiProvider } from 'tamagui';
 
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <TamaguiProvider>
+        <h1>Vite + React</h1>
+        <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+            </button>
+            <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+        </div>
+        <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+        </p>
+    </TamaguiProvider>
+  )
+}
+
+export default App
--- barracks-scanner/src/tamagui.config.ts
+++
@@ -1,6 +0,0 @@
-import { createTamagui } from '@tamagui/core';
-import { config } from '@tamagui/config/v3';
-
-const tamaguiConfig = createTamagui(config);
-
-export default tamaguiConfig;
