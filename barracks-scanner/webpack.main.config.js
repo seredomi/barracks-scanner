e9978f66f6b239295c27df68e15df9f8874238f0
seredomi <serenomdominguez@gmail.com>
2024-02-11 06:28:17 +0900

    failed attempts to change remove home_page from url

--- barracks-scanner/webpack.main.config.js
+++ barracks-scanner/webpack.main.config.js
@@ -1,9 +1,13 @@
+var path = require('path');
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
   entry: './src/main.js',
+  output: { 
+    publicPath: '/', 
+},
   // Put your normal webpack config below here
   module: {
     rules: require('./webpack.rules'),
