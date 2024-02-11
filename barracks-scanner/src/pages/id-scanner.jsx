18b8356e474098d3d04de52b5e9ba4cfa13d0e81
seredomi <serenomdominguez@gmail.com>
2024-02-11 14:41:41 +0900

    status indicator stuff

--- barracks-scanner/src/pages/id-scanner.jsx
+++ barracks-scanner/src/pages/id-scanner.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
+import { FlexGrid, Row, Column, Button, Tile, ComposedModal } from '@carbon/react';
+import { Check, Info, AlertTriangle } from 'lucide-react';
+
+
+function Indicator(props) {
+    
+    const handleStatusClick = () => { props.setReady(true); }
+
+    if (props.ready) {
+        return (
+            <Button kind='tertiary' disabled>
+                <div style={{display:'flex'}}>
+                    <Check size='20'/>
+                    <p>&nbsp;&nbsp;&nbsp;Scan back of ID when ready</p>
+                </div>
+            </Button>
+        )
+    }
+    else {
+        return (
+            <Button kind='danger' onClick={handleStatusClick}>
+                <div style={{display:'flex'}}>
+                    <AlertTriangle size='20' />
+                    <p>&nbsp;&nbsp;&nbsp;Click here before scanning!</p>
+                </div>
+            </Button>
+        )
+    }
+}
 
 export default function IdScannerPage() {
+    const [id, setId] = useState('');
+    const [ready, setReady] = useState(false);
+
+    const readyProps = { ready, setReady };
+
     return (
-        <div>
-            <h2>ID Scanner</h2>
-        </div>
+        <FlexGrid>
+            <Row>
+                <Column>
+                    <h2>ID Scanner</h2>
+                    <br/> <br/>
+                    <Indicator {...readyProps} />
+                </Column>
+            </Row>
+        </FlexGrid>
     )
 }
