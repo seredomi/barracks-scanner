2a6ada18c45c0623f1ffc4aa4b8d2150e018388a
seredomi <serenomdominguez@gmail.com>
2024-03-16 23:27:51 +0900

    window focus detection for ready status

--- src/components/pages/id-scanner.tsx
+++ src/components/pages/id-scanner.tsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import { FlexGrid, Row, Column, Button } from '@carbon/react';
 import { Check, AlertTriangle } from 'lucide-react';
+import { appWindow } from '@tauri-apps/api/window';
 
+function Indicator(scannerProps) {
+    const handleStatusClick = () => { scannerProps.setReadyToScan(true); }
 
-export function IDScannerPage() {
+    if (scannerProps.readyToScan) {
+        return (
+            <Button kind='tertiary' disabled>
+                <div style={{display:'flex'}}>
+                    <Check size='20' /> <p>Ready to scan</p>
+
+                </div>
+            </Button>
+        )
+    }
+    else {
+        return (
+            <Button kind='danger' onClick={handleStatusClick}>
+                <div style={{display:'flex'}}>
+                    <AlertTriangle size='20' /> <p>Click here before scanning!</p>
+                </div>
+            </Button>
+        )
+    }
+
+}
+
+async function handleFocusChange(setReadyToScan) {
+
+    const handleFocus = await appWindow.onFocusChanged(({ payload: focused }) => {
+        if (focused) {
+            setReadyToScan(true);
+        }
+        else {
+            setReadyToScan(false);
+        }
+    })
+
+}
+
+export function IDScannerPage(scannerProps) {
+
+    handleFocusChange(scannerProps.setReadyToScan);
+
     return (
-        <div>
-            <h2>ID Scanner</h2>
-        </div>
+        <FlexGrid>
+            <Row>
+                <Column>
+                    <h2>ID Scanner</h2>
+                    <br /> <br />
+                    <Indicator {...scannerProps} />
+                </Column>
+            </Row>
+        </FlexGrid>
     )
 }
