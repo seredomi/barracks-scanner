b6a5a5d0d974b698896c77372ff022877846fd20
seredomi <serenomdominguez@gmail.com>
2024-03-16 22:09:17 +0900

    support for sass

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,22 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(async () => ({
   plugins: [react()],
 
+  // sass support
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ['node_modules']
+      },
+    }
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+
   //
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    port: 1421,
     strictPort: true,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
