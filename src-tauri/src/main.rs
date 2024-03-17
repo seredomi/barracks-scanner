7bd357502e5616c82dad86b5ea8cf4566a3ccbc3
seredomi <serenomdominguez@gmail.com>
2024-03-17 10:05:46 +0900

    initial keystroke handler

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,9 @@
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use rdev::{listen, Event};
+use std::thread;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -7,9 +11,24 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+fn callback(event: Event) {
+
+  match event.name {
+      Some(string) => println!("User wrote {:?}", string),
+      None => (),
+  }
+}
+
+
 fn main() {
+
+    thread::spawn(|| listen(callback));
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .device_event_filter(tauri::DeviceEventFilter::Always)
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
