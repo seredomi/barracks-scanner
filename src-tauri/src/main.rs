44793e868ea3444c2d5e867e9f46e53885bf1fce
seredomi <serenomdominguez@gmail.com>
2024-03-17 12:12:01 +0900

    moving scan handler to rust background process

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,7 @@
 
 use rdev::{listen, Event};
 use std::thread;
+use std::time::SystemTime;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,18 +12,34 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn callback(event: Event) {
 
-  match event.name {
-      Some(string) => println!("User wrote {:?}", string),
-      None => (),
-  }
+fn detect_scans() {
+    let mut key_buff: Vec<String> = Vec::new();
+    let mut key_times: Vec<SystemTime> = Vec::new();
+
+    let callback = move |event: Event| {
+        match event.name {
+            Some(string) => {
+                if string == "\r" {
+                    println!("Scanned: {}", key_buff.join(""));
+                    key_buff.clear();
+                    key_times.clear();
+                } else {
+                    key_buff.push(string);
+                    key_times.push(SystemTime::now());
+                }
+            },
+            None => (),
+        }
+    };
+
+    std::thread::spawn(|| listen(callback));
 }
 
 
 fn main() {
 
-    thread::spawn(|| listen(callback));
+    thread::spawn(|| detect_scans());
 
     tauri::Builder::default()
         .device_event_filter(tauri::DeviceEventFilter::Always)
