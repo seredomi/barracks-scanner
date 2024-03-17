f2c9f26c271ae339e1f81389aea91c968abfeb94
seredomi <serenomdominguez@gmail.com>
2024-03-17 10:04:17 +0900

    better organization, clearer ui instructions

--- src/components/pages/id-scanner.tsx
+++ src/components/pages/id-scanner.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
 import { FlexGrid, Row, Column, Button } from '@carbon/react';
 import { Check, AlertTriangle } from 'lucide-react';
 import { appWindow } from '@tauri-apps/api/window';
 
-function Indicator(scannerProps) {
+async function handleFocusChange(setReadyToScan: (a: boolean) => void ) {
+    await appWindow.onFocusChanged(({ payload: focused }) => {
+        if (focused) { setReadyToScan(true); }
+        else { setReadyToScan(false); }
+    })
+}
+
+function ReadyIndicator(scannerProps: any) {
     const handleStatusClick = () => { scannerProps.setReadyToScan(true); }
 
     if (scannerProps.readyToScan) {
         return (
             <Button kind='tertiary' disabled>
                 <div style={{display:'flex'}}>
-                    <Check className='buttonIcon' /> <p>Ready to scan</p>
-
+                    <Check className='buttonIcon' /> <p>Scan back of ID when ready</p>
                 </div>
             </Button>
         )
@@ -28,14 +33,47 @@ function Indicator(scannerProps) {
 
 }
 
-async function handleFocusChange(setReadyToScan: (a: boolean) => void ) {
-    await appWindow.onFocusChanged(({ payload: focused }) => {
-        if (focused) { setReadyToScan(true); }
-        else { setReadyToScan(false); }
-    })
-}
+export function IDScannerPage(scannerProps: any) {
 
-export function IDScannerPage(scannerProps) {
+  // let keyBuffer: string[] = [];
+  // let keyTimes: number[] = [];
+
+  // window.addEventListener('keyup', (e) => { handleInput(e); });
+
+  // this function flushes the buffer to id after enter is pressed
+  // it distinguishes between human input and barcode input based on time b/w key presses
+  // the only edge case it doesn't handle is if a user mashes the keyboard while scanning
+    // function handleInput(event: KeyboardEvent) {
+
+    //     // first we push the time of the key press to the keyTimes array
+    //     keyTimes.push(performance.now());
+
+    //     // if enter
+    //     if (event.key === 'Enter') {
+
+    //     // skip all slow key presses until fast scanner-like key presses
+    //     let startIndex = 0;
+    //     for (let i = 1; i < keyTimes.length; i++) {
+    //         if (keyTimes[i] - keyTimes[i-1] > 35) {
+    //             startIndex = i;
+    //         }
+    //     }
+    //     if (startIndex < keyTimes.length - 1) {
+    //         if (startIndex !== 0) {
+    //             keyBuffer = keyBuffer.slice(startIndex);
+    //         }
+    //         // TODO: send scan to main.rs to be processed
+    //         console.log(keyBuffer);
+
+    //         keyBuffer = [];
+    //         keyTimes = [];
+    //     }
+    //     }
+    //     // if it's not enter, just push the key to the buffer
+    //     else {
+    //         keyBuffer.push(event.key);
+    //     }
+    // }
 
     handleFocusChange(scannerProps.setReadyToScan);
 
@@ -45,7 +83,7 @@ export function IDScannerPage(scannerProps) {
                 <Column>
                     <h2>ID Scanner</h2>
                     <br /> <br />
-                    <Indicator {...scannerProps} />
+                    <ReadyIndicator {...scannerProps} />
                 </Column>
             </Row>
         </FlexGrid>
