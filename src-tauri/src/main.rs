0a661a8db725ee765a56f12254de580eecdd0644
seredomi <serenomdominguez@gmail.com>
2024-03-17 13:13:47 +0900

    input differentiation works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,7 @@
 use rdev::{listen, Event};
 use std::thread;
 use std::time::SystemTime;
+use std::time::Duration;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -17,23 +18,50 @@ fn detect_scans() {
     let mut key_buff: Vec<String> = Vec::new();
     let mut key_times: Vec<SystemTime> = Vec::new();
 
+    // handle all events
     let callback = move |event: Event| {
+
+        // filter out key release events
         match event.name {
             Some(string) => {
+                // if enter is pressed, filter and flush buffer
                 if string == "\r" {
-                    println!("Scanned: {}", key_buff.join(""));
+                    // distinguish bw keypresses and scan input based on input speed
+
+                    // move start to where fast scanner-like input begins
+                    let mut start: usize = 0;
+                    for i in 1..key_buff.len() {
+                        let span = key_times[i].duration_since(key_times[i-1]);
+                        match span {
+                            Ok(d) => { if d > Duration::from_millis(25) { start = i; } },
+                            Err(_) => continue,
+                        }
+                    }
+
+                    // discard input before start
+                    if start < key_buff.len() {
+                        // return slice from start to end
+                        let discard = key_buff[..start].join("");
+                        let keep = key_buff[start..].join("");
+                        println!("Discarded: {}", discard);
+                        println!("Scanned: {}", keep);
+                    }
+                    else {
+                        println!("Discarded nada");
+                        println!("Scanned: {}", key_buff.join(""));
+                    }
                     key_buff.clear();
                     key_times.clear();
                 } else {
-                    key_buff.push(string);
                     key_times.push(SystemTime::now());
+                    key_buff.push(string);
                 }
             },
             None => (),
         }
     };
 
-    std::thread::spawn(|| listen(callback));
+    let _ = std::thread::spawn(|| listen(callback)).join();
 }
 
 
