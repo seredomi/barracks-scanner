02c4c93b51d735ac5da106566755d87e51a3aa9f
seredomi <serenomdominguez@gmail.com>
2024-03-20 23:59:16 +0900

    simplified and refactored query function process

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,95 +1,40 @@
+use crate::person::Person;
 use rusqlite::Connection;
-use tauri::AppHandle;
-use chrono::{ DateTime, Local };
-use std::collections::HashSet;
-use std::sync::mpsc;
-
-struct Person {
-    id: String,
-    rank: String,
-    last: String,
-    first: String,
-    room: String,
-    group: String,
-    leave_date: DateTime<Local>
-}
-
-fn print_person(person: &Person) {
-    println!("ID: {}", person.id);
-    println!("Rank: {}", person.rank);
-    println!("Last: {}", person.last);
-    println!("First: {}", person.first);
-    println!("Room: {}", person.room);
-    println!("Group: {}", person.group);
-    println!("Leave Date: {}", person.leave_date);
-}
+use chrono::Local;
 
 pub fn connect(filename: String) -> Result<Connection, rusqlite::Error> {
     let mut db = Connection::open(filename)?;
     Ok(db)
 }
 
-pub fn connect_and_wait(result_tx: mpsc::Sender<String>, query_rx: mpsc::Receiver<String>) {
+fn query_to_personnel(conn: &Connection, query: &str) -> Result<Vec<Person>, rusqlite::Error> {
+    let mut statement = conn.prepare(&query)?;
+    let mut rows = statement.query([])?;
+    let mut personnel = Vec::new();
+    while let Some(row) = rows.next()? {
+        personnel.push(Person::new(
+            row.get(0)?,
+            row.get(1)?,
+            row.get(2)?,
+            row.get(3)?,
+            row.get(4)?,
+            row.get(5)?,
+            Local::now()
+        ));
+    }
+    match personnel.len() {
+        0 => Ok(Vec::new()),
+        _ => Ok(personnel),
+    }
+}
 
-    let _ranks: HashSet<&str> = HashSet::from(["PVT", "PV2", "PFC", "SPC", "CPL", "SGT", "SSG", "SFC", "MSG", "1SG", "SGM", "CSM", "WO1", "CW2", "CW3", "CW4", "CW5", "2LT", "1LT", "CPT", "MAJ", "LTC", "COL", "BG", "MG", "LTG", "GEN", "GA", "CIV"]);
-    let _groups: HashSet<&str> = HashSet::from(["Resident", "Guest", "Hotel Divarty", "Rotational Unit", "Chain Of Command", "Other"]);
+// TODO: update to return a Person if found, empty Person if not
+pub fn search_for_id(db: &Connection, id: String) -> String {
 
-    let conn = Connection::open("barracks.db").unwrap();
-
-    // if the database doesn't exist, create it
-    // conn.execute(
-    //     "CREATE TABLE IF NOT EXISTS personnel (
-    //         id TEXT PRIMARY KEY,
-    //         rank TEXT CHECK(rank IN ('PVT', 'PV2', 'PFC', 'SPC', 'CPL', 'SGT', 'SSG', 'SFC', 'MSG', '1SG', 'SGM', 'CSM', 'WO1', 'CW2', 'CW3', 'CW4', 'CW5', '2LT', '1LT', 'CPT', 'MAJ', 'LTC', 'COL', 'BG', 'MG', 'LTG', 'GEN', 'GA', 'CIV')),
-    //         last TEXT,
-    //         first TEXT,
-    //         room TEXT,
-    //         group TEXT CHECK(group IN ('Resident', 'Guest', 'HotelDivarty', 'RotationalUnit', 'ChainOfCommand', 'Other')),
-    //         leaveDate DATE )"
-    // )?;
-
-    // conn.execute(
-    //     "CREATE TABLE IF NOT EXISTS logs (
-    //         id TEXT,
-    //         date DATE,
-    //         time TIME )"
-    // )?;
-
-
-    // this continually waits for queries to come in until the channel closes
-    for query in query_rx {
-        
-        let stmt = conn.prepare(&query);
-        match stmt {
-            Ok(mut stmt) => {
-                let person_iter = stmt.query_map([], |row| {
-                    Ok(Person {
-                        id: row.get(0)?,
-                        rank: row.get(1)?,
-                        last: row.get(2)?,
-                        first: row.get(3)?,
-                        room: row.get(4)?,
-                        group: row.get(5)?,
-                        // for now just do current date
-                        leave_date: Local::now(),
-                    })
-                });
-
-                match person_iter {
-                    Ok(person_iter) => {
-                        for person in person_iter {
-                            // print_person(&person.unwrap());
-                            result_tx.send("found ".to_string() + &person.unwrap().first).unwrap();
-                        }
-                    }
-                    Err(_) => {
-                        println!("Invalid query 1: ~{}~", &query);
-                        continue;
-                    }
-                }
-
-            },
-            Err(_) => println!("Invalid query 2: ~{}~", &query)
-        }
+    let query: String = "SELECT * FROM personnel WHERE id = '".to_string() + &id + "'";
+    let personnel: Vec<Person> = query_to_personnel(&db, &query).unwrap();
+    match personnel.len() {
+        0 => return "id: ".to_string() + &id + " not found",
+        _ => return "found ".to_string() + &personnel[0].get_full_name(),
     }
 }
