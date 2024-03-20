1fdbd24954274bbf2b45eadcacfc961450016c9d
seredomi <serenomdominguez@gmail.com>
2024-03-20 23:57:15 +0900

    moved input detection back to frontend

--- src/components/pages/id-scanner.tsx
+++ src/components/pages/id-scanner.tsx
@@ -1,6 +1,22 @@
 import { FlexGrid, Row, Column, Button } from '@carbon/react';
 import { Check, AlertTriangle } from 'lucide-react';
 import { appWindow } from '@tauri-apps/api/window';
+import { invoke } from '@tauri-apps/api/tauri';
+import { useState } from 'react';
+
+
+async function checkID(idArg: string)  {
+    let test: string = await invoke('check_id', { id: idArg });
+    return test;
+}
+
+function TestButton(testButtonText: String) {
+    return (
+        <Button >
+            {testButtonText}
+        </Button>
+    )
+}
 
 async function handleFocusChange(setReadyToScan: (a: boolean) => void ) {
     await appWindow.onFocusChanged(({ payload: focused }) => {
@@ -9,10 +25,10 @@ async function handleFocusChange(setReadyToScan: (a: boolean) => void ) {
     })
 }
 
-function ReadyIndicator(scannerProps: any) {
-    const handleStatusClick = () => { scannerProps.setReadyToScan(true); }
+function ReadyIndicator(readyToScan: boolean, setReadyToScan: any) {
+    const handleStatusClick = () => { setReadyToScan(true); }
 
-    if (scannerProps.readyToScan) {
+    if (readyToScan) {
         return (
             <Button kind='tertiary' disabled>
                 <div style={{display:'flex'}}>
@@ -35,7 +51,56 @@ function ReadyIndicator(scannerProps: any) {
 
 export function IDScannerPage(scannerProps: any) {
 
-    handleFocusChange(scannerProps.setReadyToScan);
+    const [ testButtonText, setTestButtonText ] = useState('testtt');
+
+    const [ readyToScan, setReadyToScan ] = useState(true);
+
+    handleFocusChange(setReadyToScan);
+
+    // scan detection
+    let keyBuffer: string[] = [];
+    let keyTimes: number[] = [];
+
+    document.removeEventListener("keydown", window.handleInput);
+
+    window.handleInput = (e: KeyboardEvent) => {
+
+        keyTimes.push(performance.now());
+
+        if (e.key === "Enter") {
+            console.log('e$');
+            // arbitrary limit, TODO: splice by 500 and work with latest
+            if (keyBuffer.length > 500) { keyBuffer = []; keyTimes = []; return; }
+
+            // work our way down from end of buffer until speed is slow like a human
+            let startIdx = keyBuffer.length;
+            for (let i = keyBuffer.length; i > 0; i--) {
+                if (keyTimes[i] - keyTimes[i-1] > 80) { break; }
+                startIdx = i;
+            }
+
+            if (startIdx === keyBuffer.length) {console.log("problem"); keyBuffer = []; keyTimes = []; return; }
+            if (startIdx > 0) {
+                keyBuffer = keyBuffer.slice(startIdx-1);
+            }
+
+            let id = keyBuffer.join('').replaceAll('Shift','');
+
+            // get result from checkID
+            checkID(id).then(
+                (result) => { setTestButtonText(result); },
+                (error) => { setTestButtonText('Error: ' + error); }
+            )
+
+            keyBuffer = [];
+            keyTimes = [];
+            }
+
+        else { keyBuffer.push(e.key); }
+    }
+
+
+    document.addEventListener("keydown", window.handleInput);
 
     return (
         <FlexGrid>
@@ -43,7 +108,9 @@ export function IDScannerPage(scannerProps: any) {
                 <Column>
                     <h2>ID Scanner</h2>
                     <br /> <br />
-                    <ReadyIndicator {...scannerProps} />
+                    {ReadyIndicator(readyToScan, setReadyToScan)}
+                    <br /> <br /> <br />
+                    {TestButton(testButtonText)}
                 </Column>
             </Row>
         </FlexGrid>
