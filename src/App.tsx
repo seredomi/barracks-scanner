c918e2e377d31e3a0e5be7536c457d316f2da69a
seredomi <serenomdominguez@gmail.com>
2024-03-20 23:58:24 +0900

    moved redux provider from App.tsx to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import "./App.scss";
 
-import { Provider } from 'react-redux';
-import store from './store';
 import Title from './components/title';
 import NavBar from './components/nav-bar.jsx';
 import PageContent from './components/page-content.jsx';
 
 export type page = "id-scanner" | "personnel" | "scan-history";
 
+declare global {
+  interface Window {
+    handleInput: (e: KeyboardEvent) => void;
+  }
+}
+
 // test to log current page
-store.subscribe(() => {
-  console.log(store.getState().page.currentPage);
-});
+
 
 const App = () => {
 
@@ -19,11 +21,9 @@ const App = () => {
 
   return (
     <>
-    <Provider store={store}>
       <Title />
       <NavBar />
       <PageContent />
-    </Provider>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { Provider, useSelector } from 'react-redux';
+import store from './store';
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
