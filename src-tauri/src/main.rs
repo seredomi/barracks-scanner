b05724ce2c6d29705d7d0e4db56a7294107ac8de
seredomi <serenomdominguez@gmail.com>
2024-03-21 00:00:35 +0900

    removed old code, added new check_id function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,42 +2,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-fn track_page(page: &str) {
-    println!("Entering page: {}", page);
-}
-
 mod state;
-mod input;
 mod database;
+mod person;
 use state::{AppState, ServiceAccess};
 use tauri::{State, Manager, AppHandle};
-use std::thread;
-use std::sync::mpsc;
+
+#[tauri::command]
+fn check_id(app_handle: AppHandle, id: String) -> String {
+    println!("Checking ID: {}", id);
+    return app_handle.db(|db: &rusqlite::Connection| database::search_for_id(db, id));
+}
 
 fn main() {
-
-    let (scanner_tx, database_rx) = mpsc::channel();
-    // let personnel_tx = scanner_tx.clone();
-    // let logs_tx = scanner_tx.clone();
-    let (database_tx, scanner_rx) = mpsc::channel();
-
-    // connect to database and await calls
-    thread::spawn(move || database::connect_and_wait(database_tx, database_rx));
-
-    // TODO: only run when the ID Scanner page is open
-    thread::spawn(move || input::await_scan(scanner_tx, scanner_rx));
 
     tauri::Builder::default()
         .device_event_filter(tauri::DeviceEventFilter::Always)
         .manage(AppState { db: Default::default() })
-        .invoke_handler(tauri::generate_handler![ greet, track_page])
+        .invoke_handler(tauri::generate_handler![ check_id])
         .setup(|app| {
             let handle = app.handle();
             let app_state: State<AppState> = handle.state();
