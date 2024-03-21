508cb229f1d66ec81866b9af082e9999383c94f0
seredomi <serenomdominguez@gmail.com>
2024-03-21 21:50:38 +0900

    constructor from Person object that backend returns

---
+++ src/classes/person.ts
@@ -0,0 +1,27 @@
+type rankType = "PVT" | "PV2" | "PFC" | "SPC" | "CPL" | "SGT" | "SSG" | "SFC" | "MSG" | "1SG" | "SGM" | "CSM" | "WO1" | "CW2" | "CW3" | "CW4" | "CW5" | "2LT" | "1LT" | "CPT" | "MAJ" | "LTC" | "COL" | "BG" | "MG" | "LTG" | "GEN" | "GA";
+type groupType = "Resident" | "Hotel Divarty" | "Rotational Unit" | "Chain of Command" | "Guest"; 
+
+class Person {
+    id: string
+    rank: rankType;
+    last: string;
+    first: string;
+    room: string;
+    group: groupType;
+    // TODO: change to date type
+    leaveDate: string;
+    found: boolean;
+
+    constructor(personObj: any) {
+        this.id = personObj.id;
+        this.rank = personObj.rank;
+        this.last = personObj.last;
+        this.first = personObj.first;
+        this.room = personObj.room;
+        this.group = personObj.group;
+        this.leaveDate = personObj.leave_date;
+        this.found = personObj.found;
+    }
+}
+
+export default Person;
