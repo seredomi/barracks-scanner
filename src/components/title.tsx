7b852c34bddae1c28e958abf9349fb4edfea47f7
seredomi <serenomdominguez@gmail.com>
2024-03-22 11:17:04 +0900

    dark title bar

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,4 +1,4 @@
-
+import { Theme } from '@carbon/react';
 import { Header, HeaderName, HeaderGlobalBar, HeaderGlobalAction } from '@carbon/react';
 import { appWindow } from '@tauri-apps/api/window';
 import { Close } from '@carbon/icons-react'
@@ -6,16 +6,18 @@ import { Close } from '@carbon/icons-react'
 const Title = () => {
   const buildingNo = 'P6070';
   return (
-    <Header aria-label='window title' data-tauri-drag-region className='window-title' >
-        <HeaderName data-tauri-drag-region  prefix={buildingNo} >
-            Barracks Personnel Identifier
-        </HeaderName>
-        <HeaderGlobalBar data-tauri-drag-region >
-            <HeaderGlobalAction aria-label='Close' onClick={() => appWindow.close()}>
-                <Close size={20} />
-            </HeaderGlobalAction>
-        </HeaderGlobalBar>
-    </Header>
+    <Theme theme='g90'>
+      <Header aria-label='window title' data-tauri-drag-region className='window-title' >
+          <HeaderName data-tauri-drag-region  prefix={buildingNo} >
+              Barracks Personnel Identifier
+          </HeaderName>
+          <HeaderGlobalBar data-tauri-drag-region >
+              <HeaderGlobalAction aria-label='Close' onClick={() => appWindow.close()}>
+                  <Close size={20} />
+              </HeaderGlobalAction>
+          </HeaderGlobalBar>
+      </Header>
+    </Theme>
   )
 }
 
