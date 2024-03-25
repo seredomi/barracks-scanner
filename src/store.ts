d8a80407ea7982000089698b8e2be79e4abaf483
seredomi <serenomdominguez@gmail.com>
2024-03-25 18:46:56 +0900

    async thunk for fetching personnel

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { invoke } from '@tauri-apps/api/tauri';
 
 const pageSlice = createSlice({
     name: 'page',
@@ -10,12 +11,43 @@ const pageSlice = createSlice({
     },
 });
 
+type PersonRow = {id: string, rank: string, last: string, first: string, group: string};
+const emptyPersonnel: PersonRow[] = [];
+
+export const queryAll = createAsyncThunk(
+    'personnel/fetchAll',
+    async () => {
+        const rows: PersonRow[] = await invoke('query_all')
+        return rows
+    },
+)
+
+const personnelSlice = createSlice({
+    name: 'personnel',
+    initialState: {personnelData: emptyPersonnel},
+    reducers: { },
+    extraReducers: (builder) => {
+        builder.addCase(queryAll.fulfilled, (state, action) => {
+            state.personnelData = action.payload
+        }),
+        builder.addCase(queryAll.pending, (state) => {
+            state.personnelData = emptyPersonnel
+        }),
+        builder.addCase(queryAll.rejected, (state) => {
+            state.personnelData = emptyPersonnel
+        })
+    }
+});
+
 export const { setCurrentPage } = pageSlice.actions;
 
 const store = configureStore({
     reducer: {
         page: pageSlice.reducer,
+        personnel: personnelSlice.reducer,
     },
 });
+
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
