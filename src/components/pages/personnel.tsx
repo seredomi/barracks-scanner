dfde3140aeda07bde62179b325ea3c9b71a28515
seredomi <serenomdominguez@gmail.com>
2024-03-28 19:09:06 +0900

    working details view per person

--- src/components/pages/personnel.tsx
+++ src/components/pages/personnel.tsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
-import { DataTable, TableContainer, TableToolbar, TableToolbarContent, TableToolbarAction, TableToolbarSearch, TableToolbarMenu, Button, Table, TableHead, TableRow, TableHeader, TableBody, TableCell, } from '@carbon/react';
-import { AddLarge } from '@carbon/icons-react';
+import { DataTable, TableContainer, TableToolbar, TableToolbarContent, TableToolbarSearch,
+        Table, TableHead, TableRow, TableHeader, TableBody, TableCell,
+        Button, OverflowMenuItem, OverflowMenu, Modal, TextInput, Select, SelectItemGroup, SelectItem} from '@carbon/react';
+import { AddLarge, OverflowMenuVertical } from '@carbon/icons-react';
 import { useSelector, useDispatch } from 'react-redux';
-import { queryAll } from '../../store';
-import { AppDispatch } from '../../store';
+import { queryAll, AppDispatch, Person, emptyPerson } from '../../store';
+import { checkID } from './id-scanner';
 
 
 type Header = {key: string, header: string};
-type Row = {id: string, rank: string, last: string, first: string, group: string};
 
 const headers = [
     { key: 'rank', header: 'Rank' },
@@ -16,47 +17,167 @@ const headers = [
     { key: 'group', header: 'Group' },
 ]
 
-const PersonnelTableHeaders = (headers: Header[], rows: Row[]) => {
-    if (rows.length === 0) { return null; }
+
+const PersonnelTable = (personnel: Person[], headers: Header[], onSearchChange: any, setSelectedPerson, setDetailsOpen) => {
+
     return (
-        <TableHead>
-            <TableRow>
-                {headers.map((header) => (
-                    <TableHeader >
-                        {header.header}
-                    </TableHeader>
-                ))}
-            </TableRow>
-        </TableHead>
+        <DataTable rows={personnel} headers={headers} >
+            {({ rows, headers }) => 
+
+            <TableContainer>
+                <TableToolbar>
+                    <TableToolbarContent>
+                        <TableToolbarSearch onChange = {onSearchChange} />
+                        <Button renderIcon={AddLarge} onClick={() => {
+                            setDetailsOpen(true)
+                            setSelectedPerson(personnel[0])
+                            }}>
+                            New
+                        </Button>
+                    </TableToolbarContent>
+                </TableToolbar>
+                <Table overflowMenuOnHover={true} isSortable={true}>
+                <TableHead>
+                    <TableRow>
+                        {headers.map((header) => (
+                            <TableHeader>
+                                {header.header}
+                            </TableHeader>
+                        ))}
+                        <TableHeader />
+                    </TableRow>
+                </TableHead>
+                    <TableBody>
+                        {rows.map(row =>
+                        <TableRow key={row.id} >
+                            {row.cells.map(cell =>
+                            <TableCell key={cell.id}> {cell.value} </TableCell>)}
+                            <TableCell className="bx--table-column-menu">
+                                <OverflowMenu  size='sm' light flipped onClick={() => {
+                                    checkID(row.id).then(
+                                        (result) => {
+                                            setSelectedPerson(result);
+                                            console.log(result);
+                                            setDetailsOpen(true)
+                                        },
+                                        (error) => { console.log("error: " + error); }
+            )
+                                } } >
+                                </OverflowMenu>
+                            </TableCell>
+                        </TableRow>)}
+                    </TableBody>
+                </Table>
+            </TableContainer> }
+        </DataTable> 
     )
 }
 
-const PersonnelTable = (rows: Row[], headers: Header[], onSearchChange: any) => {
+
+const PersonDetails = (person: any, detailsOpen: boolean, setDetailsOpen: (arg0: boolean) => void) => {
+    const [ editMode, setEditMode ] = useState(false);
 
     return (
-        <TableContainer>
-            <TableToolbar>
-                <TableToolbarContent>
-                    <TableToolbarSearch onChange = {onSearchChange} />
-                    <Button renderIcon={AddLarge} onClick={() => console.log("Add button clicked")}>
-                        New
-                    </Button>
-                </TableToolbarContent>
-            </TableToolbar>
-            <Table >
-                {PersonnelTableHeaders(headers, rows)}
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow >
-                            <TableCell key={row.id}> {row.rank} </TableCell>
-                            <TableCell key={row.id}> {row.last} </TableCell>
-                            <TableCell key={row.id}> {row.first} </TableCell>
-                            <TableCell key={row.id}> {row.group} </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Modal
+            open={detailsOpen}
+            onRequestClose={() => {
+                setDetailsOpen(false)
+                setEditMode(false)
+            }}
+            modalHeading={ (editMode ? "Edit" : "View" ) + " details"}
+            secondaryButtonText="Cancel"
+            primaryButtonText={ editMode ? "Save" : "Edit"}
+        >
+            <TextInput
+                id='id-input'
+                labelText="ID (Scan back of ID)"
+                disabled={!editMode}
+                value={person.id}
+                style={{marginBottom: '1rem'}}
+            />
+
+            <div id='rank-name=input' className='formGroup' style={{marginBottom:'1rem'}}>
+                <Select
+                    id='rank-select'
+                    labelText="Rank"
+                    disabled={!editMode}
+                    value={person.rank}
+                >
+                    <SelectItemGroup label="Enlisted">
+                        <SelectItem value="PVT" text="PVT" />
+                        <SelectItem value="PV2" text="PV2" />
+                        <SelectItem value="PFC" text="PFC" />
+                        <SelectItem value="SPC" text="SPC" />
+                        <SelectItem value="CPL" text="CPL" />
+                        <SelectItem value="SGT" text="SGT" />
+                        <SelectItem value="SSG" text="SSG" />
+                        <SelectItem value="SFC" text="SFC" />
+                        <SelectItem value="MSG" text="MSG" />
+                        <SelectItem value="1SG" text="1SG" />
+                        <SelectItem value="SGM" text="SGM" />
+                        <SelectItem value="CSM" text="CSM" />
+                    </SelectItemGroup>
+                    <SelectItemGroup label="Warrant Officer">
+                        <SelectItem value="WO1" text="WO1" />
+                        <SelectItem value="CW2" text="CW2" />
+                        <SelectItem value="CW3" text="CW3" />
+                        <SelectItem value="CW4" text="CW4" />
+                        <SelectItem value="CW5" text="CW5" />
+                    </SelectItemGroup>
+                    <SelectItemGroup label="Officer">
+                        <SelectItem value="2LT" text="2LT" />
+                        <SelectItem value="1LT" text="1LT" />
+                        <SelectItem value="CPT" text="CPT" />
+                        <SelectItem value="MAJ" text="MAJ" />
+                        <SelectItem value="LTC" text="LTC" />
+                        <SelectItem value="COL" text="COL" />
+                        <SelectItem value="BG" text="BG" />
+                        <SelectItem value="MG" text="MG" />
+                        <SelectItem value="LTG" text="LTG" />
+                        <SelectItem value="GEN" text="GEN" />
+                        <SelectItem value="GA" text="GA" />
+                    </SelectItemGroup>
+                    <SelectItemGroup label="Other">
+                        <SelectItem value="CIV" text="CIV" />
+                        <SelectItem value="CTR" text="CTR" />
+                    </SelectItemGroup>
+                </Select>
+                <TextInput
+                    id='last-name-input'
+                    labelText="Last name"
+                    disabled={!editMode}
+                    value={person.last}
+                />
+                <TextInput
+                    id='first-name-input'
+                    labelText="First name"
+                    disabled={!editMode}
+                    value={person.first}
+                />
+            </div>
+
+            <div id='room-group-date-input' className='formGroup'>
+                <TextInput
+                    id='group-input'
+                    labelText="Group"
+                    disabled={!editMode}
+                    value={person.group}
+                />
+                <TextInput
+                    id='room-input'
+                    labelText="Room"
+                    disabled={!editMode}
+                    value={person.room}
+                />
+                <TextInput
+                    id='leave-date-input'
+                    labelText="Leave Date"
+                    disabled={!editMode}
+                    value={person.leave_date}
+                />
+            </div>
+
+        </Modal>
     )
 }
 
@@ -65,7 +186,7 @@ export function PersonnelPage() {
 
     const [search, setSearch] = useState('');
 
-    const onSearchChange = (e) => {
+    const onSearchChange = (e: any) => {
         setSearch(e.target.value);
         console.log(e.target.value);
     }
@@ -75,13 +196,17 @@ export function PersonnelPage() {
     }, [search]);
 
 
-    const rows = useSelector((state: any) => state.personnel.personnelData);
+    const personnel = useSelector((state: any) => state.personnel.personnelData);
+
+    const [ detailsOpen, setDetailsOpen ] = useState(false);
+    const [ selectedPerson, setSelectedPerson ] = useState(emptyPerson);
 
     return (
         <div>
             <h2>Personnel</h2>
             <br/>
-            {PersonnelTable(rows, headers, onSearchChange)}
+            {PersonDetails(selectedPerson, detailsOpen, setDetailsOpen)}
+            {PersonnelTable(personnel, headers, onSearchChange, setSelectedPerson, setDetailsOpen )}
         </div>
     )
 }
