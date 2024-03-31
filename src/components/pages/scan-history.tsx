75f71410611b6c40120e9d2e6eeee7962119f6a5
seredomi <serenomdominguez@gmail.com>
2024-03-31 23:03:29 +0900

    pulls current scan history - date range not implemented yet

--- src/components/pages/scan-history.tsx
+++ src/components/pages/scan-history.tsx
@@ -1,9 +1,100 @@
+import { useState, useEffect } from 'react';
+import { DataTable, TableContainer, TableToolbar, TableToolbarContent, TableToolbarSearch,
+    Table, TableHead, TableRow, TableHeader, TableBody, TableCell, DatePicker, DatePickerInput } from '@carbon/react';
+import { useSelector, useDispatch } from 'react-redux';
+import { queryLogs, AppDispatch, emptyLog, LogQuery } from '../../store';
+import { formatCalendarDate } from '../../classes/date';
 
+const headers = [
+    { key: 'date', header: 'Date' },
+    { key: 'time', header: 'Time' },
+    { key: 'rank', header: 'Rank' },
+    { key: 'last', header: 'Last Name' },
+    { key: 'first', header: 'First Name' },
+]
+
+const LogsTable = (props: any) => {
+
+    return (
+        <DataTable rows={props.logs} headers={headers} >
+            {({ rows, headers }) => 
+
+            <TableContainer>
+                <TableToolbar>
+                    <TableToolbarContent>
+                        <TableToolbarSearch onChange = { (e) => props.setSearch(e.target.value) } />
+                    </TableToolbarContent>
+                    <DatePicker 
+                        datePickerType="range"
+                        dateFormat='Y-m-d'
+                        >
+                        <DatePickerInput 
+                            id="date-picker-input-id-start" 
+                            labelText=" "
+                            onChange={(e) => props.setStartDate(formatCalendarDate(e.toLocaleString()))}
+                            placeholder="Start" />
+                        <DatePickerInput 
+                            id="date-picker-input-id-start" 
+                            labelText=" "
+                            onChange={(e) => props.setEndDate(formatCalendarDate(e.toLocaleString()))}
+                            placeholder="End" />
+
+                    </DatePicker>
+                </TableToolbar>
+                <Table overflowMenuOnHover={true} isSortable={true}>
+                <TableHead>
+                    <TableRow>
+                        {headers.map((header: any) => (
+                            <TableHeader>
+                                {header.header}
+                            </TableHeader>
+                        ))}
+                        <TableHeader />
+                    </TableRow>
+                </TableHead>
+                    <TableBody>
+                        {rows.map(row =>
+                        <TableRow key={row.id} >
+                            {row.cells.map(cell =>
+                            <TableCell key={cell.id}> {cell.value} </TableCell>)}
+                        </TableRow>)}
+                    </TableBody>
+                </Table>
+            </TableContainer> }
+        </DataTable> 
+    )
+}
 
 export function ScanHistoryPage() {
+
+    const [ search, setSearch ] = useState<string>("");
+    const [ startDate, setStartDate ] = useState<string>('2023-01-01');
+    const [ endDate, setEndDate ] = useState<string>(formatCalendarDate(new Date(Date.now()).toLocaleString()));
+    const [ query, setQuery ] = useState<LogQuery>({ search: search, startDate: startDate, endDate: endDate });
+
+    useEffect (() => {
+        setQuery({ search: search, startDate: startDate, endDate: endDate });
+    }, [search, startDate, endDate]);
+
+    const logs = useSelector((state: any) => state.logs.logsData);
+
+    const tableProps = {
+        logs: logs,
+        headers: headers,
+        setSearch: setSearch,
+        setStartDate: setStartDate,
+        setEndDate: setEndDate,
+    }
+
+    const dispatch = useDispatch<AppDispatch>();
+    const refresh = () => { dispatch(queryLogs(query)) }
+    useEffect(() => { refresh(); }, [query]);
+
     return (
         <div>
             <h2>Scan History</h2>
+            <br/>
+            {LogsTable(tableProps)}
         </div>
     )
 }
--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import { configureStore, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { invoke } from '@tauri-apps/api/tauri';
 
+// current page ---------------------------
 const pageSlice = createSlice({
     name: 'page',
     initialState: { currentPage: 'id-scanner', },
@@ -11,6 +12,7 @@ const pageSlice = createSlice({
     },
 });
 
+// personnel -------------------------------
 export type Person = {
     id: string, 
     rank: string, 
@@ -21,6 +23,7 @@ export type Person = {
     leave_date: string,
     found: boolean,
 } | {};
+
 export const emptyPerson: Person = {
     id: "",
     rank: "",
@@ -32,10 +35,11 @@ export const emptyPerson: Person = {
     found: false,
 }
 
-export const queryAll = createAsyncThunk(
-    'personnel/fetchAll',
+
+export const queryPersonnel = createAsyncThunk(
+    'personnel/queryAll',
     async (search: String) => {
-        const rows: Person[] = await invoke('query_all', { search: search })
+        const rows: Person[] = await invoke('query_personnel', { search: search })
         return rows;
     },
 )
@@ -45,27 +49,81 @@ const personnelSlice = createSlice({
     initialState: {personnelData: [emptyPerson]},
     reducers: { },
     extraReducers: (builder) => {
-        builder.addCase(queryAll.fulfilled, (state, action) => {
+        builder.addCase(queryPersonnel.fulfilled, (state, action) => {
             state.personnelData = action.payload
         }),
-        builder.addCase(queryAll.pending, (state) => {
+        builder.addCase(queryPersonnel.pending, (state) => {
             state.personnelData = []
         }),
-        builder.addCase(queryAll.rejected, (state) => {
+        builder.addCase(queryPersonnel.rejected, (state) => {
             state.personnelData = []
         })
     }
 });
 
-export const { setCurrentPage } = pageSlice.actions;
 
+// logs -------------------------------------
+export type Log = {
+    id: string,
+    rank: string,
+    last: string,
+    first: string,
+    date: string,
+    time: string,
+} | {};
+
+export const emptyLog: Log = {
+    id: "",
+    rank: "",
+    last: "",
+    first: "",
+    date: "",
+    time: "",
+}
+
+export type LogQuery = {
+    search: String,
+    startDate: String,
+    endDate: String,
+}
+
+export const queryLogs = createAsyncThunk(
+    'logs/queryAll',
+    async (query: LogQuery) => {
+        const rows: Log[] = await invoke(
+            'query_logs', { search: query.search, startDate: query.startDate, endDate: query.endDate})
+        return rows;
+    },
+)
+
+const logsSlice = createSlice({
+    name: 'logs',
+    initialState: {logsData: [emptyLog]},
+    reducers: { },
+    extraReducers: (builder) => {
+        builder.addCase(queryLogs.fulfilled, (state, action) => {
+            state.logsData = action.payload
+        }),
+        builder.addCase(queryLogs.pending, (state) => {
+            state.logsData = []
+        }),
+        builder.addCase(queryLogs.rejected, (state) => {
+            state.logsData = []
+        })
+    }
+});
+
+
+
+// store ------------------------------------
 const store = configureStore({
     reducer: {
         page: pageSlice.reducer,
         personnel: personnelSlice.reducer,
+        logs: logsSlice.reducer,
     },
 });
 
+export const { setCurrentPage } = pageSlice.actions;
 export type AppDispatch = typeof store.dispatch;
-
 export default store;
